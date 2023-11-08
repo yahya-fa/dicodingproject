556ad273c684918e84556161bcceb9db75709a8a
Yahya FA <73689554+yahya-fa@users.noreply.github.com>
2023-11-08 18:21:06 +0700

    Update bikesharing.py

--- bikesharing.py
+++ bikesharing.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('F:\BELAJAR dicoding\dicodingproject/hour.csv')
+df = pd.read_csv('F:\BELAJAR dicoding\dicodingproject\hour.csv')
 
 df = df.rename(columns= {'weathersit': 'weather',
                                    'yr': 'year',
